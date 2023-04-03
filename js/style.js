4ccd7f15b8ac005cae41487cb6a5edb57a171fb2
itsnobita <47901403+itsnobita@users.noreply.github.com>
2023-04-03 11:54:19 +0530

    Update style.js

--- js/style.js
+++ js/style.js
@@ -4,14 +4,25 @@ function my(){
     if(x==""){
        alert("Please enter valid city name");
     }else{
-        
+        if(localStorage.getItem("xesclub")!==null){
+            let txt="Vacancy in "+x;
+ 
+    document.getElementById("citty").innerHTML=txt;
+    document.getElementById("citty").style.fontSize = "x-large";
+    document.getElementById("vacancy").innerHTML=localStorage.getItem("xesclub");
+    document.getElementById("vacancy").style.fontSize = "x-large";
+    document.getElementById("myy").disabled = true;
+        }
+        else{
     let txt="Vacancy in "+x;
  
     document.getElementById("citty").innerHTML=txt;
     document.getElementById("citty").style.fontSize = "x-large";
     let vac=Math.floor(Math.random() * 6) + 1;
+            localStorage.setItem("xesclub",vac);
     document.getElementById("vacancy").innerHTML=vac;
     document.getElementById("vacancy").style.fontSize = "x-large";
     document.getElementById("myy").disabled = true;
+        }
     }
     }
